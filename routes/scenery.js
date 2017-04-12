09eb395607fb9940d24275951261d506a801c6f3
wangyiping <61780374@163.com>
2017-04-12 17:24:07 +0800

    feature:introduce分页显示

--- routes/scenery.js
+++ routes/scenery.js
@@ -10,17 +10,30 @@ router.get('', (req, res) => {
 })
 
 // 介绍
-router.get('/introduce', (req, res) => {
-    db.Scenery.find().sort().limit(6).exec((err, data) => {
-        res.render('scenery/introduce', {
-            sceneries: data.map(m => {
-                m = m.toObject();
-                m.id = m._id.toString();
-                delete m._id;
-                return m;
-            })
-        });
-    })
+router.get('/introduce/(:page)?', (req, res) => {
+    var page = req.params.page;
+    page = page || 1;
+    page = parseInt(page);
+    db.Scenery.find().count((err, count) => {
+        if (err) {
+            
+        } else {
+            var pageCount = Math.ceil(count / 6);
+            page = page > pageCount ? pageCount : page;
+            page = page < 1 ? 1 : page;
+            db.Scenery.find().skip((page - 1) * 6).sort().limit(6).exec((err, data) => {
+                res.render('scenery/introduce', {
+                    page, pageCount,
+                    sceneries: data.map(m => {
+                        m = m.toObject();
+                        m.id = m._id.toString();
+                        delete m._id;
+                        return m;
+                    })
+                });
+            });
+        }
+    });
 })
 
 // gallery
