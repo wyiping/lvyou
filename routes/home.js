7936f2969c42dc66211680b1304efc72d9023608
wangyiping <61780374@163.com>
2017-06-06 15:05:20 +0800

    update:添加introduce页面

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,19 @@ const db = require('../modules/db')
 //创建一个路由对象。
 var router = express.Router()
 
+// 获取显示的页数，最多5页
+function getPages(page, pageCount) {
+    var pages = [page];
+    var left = page - 1;
+    var right = page + 1;
+    // 左右两边各加1个页码,直到页码够5个或左边到1 右边到总页数
+    while (pages.length < 5 && (left >= 1 || right <= pageCount)) {
+        if (left > 0) pages.unshift(left--);
+        if (right <= pageCount) pages.push(right++);
+    }
+    return pages;
+}
+
 // 主页
 router.get('', (req, res) => {
     res.render('home')
@@ -31,6 +44,31 @@ router.get('/introduce/(:page)?', (req, res) => {
             db.Scenery.find().skip((page - 1) * 6).sort().limit(6).exec((err, data) => {
                 res.render('home/introduce', {
                     page, pageCount,
+                    sceneries: data.map(m => {
+                        m = m.toObject();
+                        m.id = m._id.toString();
+                        delete m._id;
+                        return m;
+                    })
+                });
+            });
+        }
+    });
+})
+router.get('/introduce/list/(:page)?', (req, res) => {
+    var page = req.params.page;
+    page = page || 1;
+    page = parseInt(page);
+    db.Scenery.find().count((err, count) => {
+        if (err) {
+
+        } else {
+            var pageCount = Math.ceil(count / 6);
+            page = page > pageCount ? pageCount : page;
+            page = page < 1 ? 1 : page;
+            db.Scenery.find().skip((page - 1) * 6).sort().limit(6).exec((err, data) => {
+                res.render('detail/introduce', {
+                    page, pageCount, pages: getPages(page, pageCount),
                     sceneries: data.map(m => {
                         m = m.toObject();
                         m.id = m._id.toString();
