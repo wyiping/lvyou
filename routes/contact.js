49f1b12243d638aa995c753ebcea1cd49a201c89
wangyiping <61780374@163.com>
2017-06-06 23:04:24 +0800

    update:留言板分页

--- routes/contact.js
+++ routes/contact.js
@@ -3,12 +3,43 @@ const express = require("express")
 const db = require('../modules/db')
 //创建一个路由对象。
 var router = express.Router()
-
+// 获取显示的页数，最多5页
+function getPages(page, pageCount) {
+    var pages = [page];
+    var left = page - 1;
+    var right = page + 1;
+    // 左右两边各加1个页码,直到页码够5个或左边到1 右边到总页数
+    while (pages.length < 5 && (left >= 1 || right <= pageCount)) {
+        if (left > 0) pages.unshift(left--);
+        if (right <= pageCount) pages.push(right++);
+    }
+    return pages;
+}
 // 留言板
-router.get('/contact', (req, res) => {
-    db.Contact.find((err, data) => {
-        res.render('detail/contact', { contact: data })
-    })
+router.get('/contact/(:page)?', (req, res) => {
+    var page = req.params.page;
+    page = page || 1;
+    page = parseInt(page);
+    db.Contact.find().count((err, count) => {
+        if (err) {
+
+        } else {
+            var pageCount = Math.ceil(count / 6);
+            page = page > pageCount ? pageCount : page;
+            page = page < 1 ? 1 : page;
+            db.Contact.find().skip((page - 1) * 6).sort().limit(6).exec((err, data) => {
+                res.render('detail/contact', {
+                    page, pageCount, pages: getPages(page, pageCount),
+                    contact: data.map(m => {
+                        m = m.toObject();
+                        m.id = m._id.toString();
+                        delete m._id;
+                        return m;
+                    })
+                });
+            });
+        }
+    });
 })
 router.post('/contact', (req, res) => {
     req.body.createTime = new Date();
